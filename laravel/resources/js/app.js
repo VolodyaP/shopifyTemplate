b66656623e0e5b1ae48ec63080ade4eff401626d
Volodimir <weber.uk.lv@gmail.com>
2024-07-11 19:24:22 +0300

    Add laravel vite

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+const titleBarOptions = {
+    title: 'My page title',
+};
+const modalOptions = {
+    title: 'My Modal',
+    message: 'Hello world!',
+};
+
+const myTitleBar = actions.TitleBar.create(app, titleBarOptions);
+const myModal = actions.Modal.create(app, modalOptions);
+myModal.dispatch(actions.Modal.Action.OPEN);
